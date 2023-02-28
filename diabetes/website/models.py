234296b76909f89b1ae3488944e38ee280196313
Joseph Owino <89513250+josephotienoowino@users.noreply.github.com>
2023-02-28 21:33:03 +0300

    Update models.py
    changes

--- diabetes/website/models.py
+++ diabetes/website/models.py
@@ -1,52 +1,20 @@
-from website import create_app
-from flask import Blueprint, render_template, request, flash, redirect, url_for
-import pandas as pd
-import pickle
-from sklearn.linear_model import LogisticRegression
-from flask_login import login_required, current_user
-
-
-clf = LogisticRegression()
-app = create_app()
-
-model = pickle.load(open("model.pkl", "rb"))
-    
-@app.route('/', methods=['GET', 'POST'])
-@login_required
-def home():
-    prediction = request.args.get('pred')
-    return render_template("home.html", user=current_user, pred=prediction)
-
-@app.route('/results')
-@login_required
-def results():
-    prediction = request.args.get('pred')
-    return render_template("results.html", user=current_user, pred=prediction)
-
-@app.route('/predict', methods=['POST', 'GET'])
-def predict():
-    text1 = request.form['1']
-    text2 = request.form['2']
-    text3 = request.form['3']
-    text4 = request.form['4']
-    text5 = request.form['5']
-    text6 = request.form['6']
-    text7 = request.form['7']
-    text8 = request.form['8']
-
-    row_df = pd.DataFrame([pd.Series([text1,text2,text3, text4, text5, text6,text7,text8])])
-
-    prediction = model.predict_proba(row_df)
-    output = '{0:.{1}f}'.format(prediction[0][1], 2)
-    output = float(output) * 100
-
-    if output > 50.0:
-        return redirect(url_for('results', pred=f'You have chance of having diabetes.\nProbability of having Diabetes is {output}%'))
-    else:
-        return redirect(url_for('results', pred=f'You are safe.\n Probability of having diabetes is {output}%'))
+from .import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+  
