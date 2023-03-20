f94276e7d175ccd5821dfaafe75a508bfdce9aec
Joseph Owino <89513250+josephotienoowino@users.noreply.github.com>
2023-03-20 04:23:50 +0300

    Update views.py

--- diabetes/website/views.py
+++ diabetes/website/views.py
@@ -1,11 +1,22 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, url_for, redirect
 from flask_login import login_required, current_user
-from .models import Note
-from . import db
-import json
+
+
+
 
 views = Blueprint('views', __name__)
 
+@views.route('/', methods=['GET', 'POST'])
+def welcome():
+    if current_user.is_authenticated:
+        if request.path == url_for('views.home'):
+            # user is already on the home page, no need to redirect
+            return render_template('home.html', user=current_user)
+        else:
+            # redirect to home page
+            return redirect(url_for('views.home'))
+    else:
+        return render_template('welcome.html')
 
 @views.route('/', methods=['GET', 'POST'])
 @login_required
@@ -13,6 +24,9 @@ def home():
    
 #returns home 
     return render_template("home.html", user=current_user)
+
+
+
 
 
 
