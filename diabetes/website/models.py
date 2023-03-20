ed0fbe0b70997909f9c4b1aa2a488a29507439d5
Joseph Owino <89513250+josephotienoowino@users.noreply.github.com>
2023-03-20 04:25:20 +0300

    Update models.py

--- diabetes/website/models.py
+++ diabetes/website/models.py
@@ -2,9 +2,6 @@ from .import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-
-
-
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
@@ -17,4 +14,11 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-  
+    
+
+class Prediction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    prediction = db.Column(db.String(10))
+    probability = db.Column(db.Float)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
