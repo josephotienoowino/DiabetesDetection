b189665622ef4f82e3cd1be873d1bc92b323d626
Joseph Owino <89513250+josephotienoowino@users.noreply.github.com>
2023-03-20 04:27:43 +0300

    Update __init__.py
    ed

--- diabetes/website/__init__.py
+++ diabetes/website/__init__.py
@@ -27,6 +27,7 @@ def create_app():
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
+    
 
     @login_manager.user_loader
     def load_user(id):
